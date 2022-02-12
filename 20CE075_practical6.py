e6681b61b96fec36e592a721da6ba4d131008e39
Morningstar000 <96479756+Morningstar000@users.noreply.github.com>
2022-02-12 22:04:40 +0530

    Update 20CE075_practical6.py

--- 20CE075_practical6.py
+++ 20CE075_practical6.py
@@ -3,7 +3,7 @@
 #  Aim : You are given n words. Some words may repeat. For each word, output its number of occurrences.
 #  The output order should correspond with the input order of appearance of the word.
 #  See the sample input/output for clarification.
-# 
+#  https://github.com/Morningstar000/Python-Practical-6
 
 t = int(input())
 str1 = ""
